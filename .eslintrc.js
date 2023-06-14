563b5558f8a6aa36e04868a7936253a1ff34aacc
Hector Matus <hector@koibanx.com>
2023-06-14 10:51:14 -0600

    Added absolute path

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+      requireConfigFile: false,
+    },
+  },
 };
