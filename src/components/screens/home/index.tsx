60076a62019ebcefc74e8b1b17baff72a006a911
Hector Matus <hector@koibanx.com>
2023-06-16 11:22:45 -0600

    Added home pokemon flatlist

--- src/components/screens/home/index.tsx
+++ src/components/screens/home/index.tsx
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import Screen from 'src/components/elements/Screen';
-import {MainRootStack} from 'src/components/navigation/mainNav';
-import MainButton from 'src/components/elements/Button/MainButton';
+
 import {useAppDispatch} from 'src/redux/hooks';
 import {setLoggedOut} from 'src/redux/auth/slice';
+import {FlatList} from 'native-base';
+import {BottomRootTab} from 'src/components/navigation/tabNav';
+import {service} from 'src/services/service';
+import {pokemonService} from 'src/services/pokemon/pokemonService';
+import {FlatListProps} from 'react-native';
+import PokemonCard from 'src/components/elements/Card/PokemonCard';
+import {API_URL} from '@env';
+import {styles} from './styles';
 
-type Props = NativeStackScreenProps<MainRootStack, 'Home'>;
+type Props = NativeStackScreenProps<BottomRootTab, 'Home'>;
 const Home = ({}: Props) => {
-  const dispatch = useAppDispatch();
-
-  const handlePress = () => {
-    dispatch(setLoggedOut());
+  const [data, setData] = useState([]);
+  const getAllPokemons = async (limit: number) => {
+    try {
+      const result = await pokemonService.getAllPokemons(limit);
+      setData(result.results);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  useEffect(() => {
+    getAllPokemons(10);
+  }, []);
+
   return (
     <Screen>
-      <MainButton text="Cerrar sesion" onPress={handlePress} />
+      <>
+        <FlatList
+          columnWrapperStyle={styles.contentFlatlist}
+          data={data}
+          numColumns={3}
+          renderItem={({item, index}) => (
+            <PokemonCard index={index} item={item} />
+          )}
+        />
+      </>
     </Screen>
   );
 };
